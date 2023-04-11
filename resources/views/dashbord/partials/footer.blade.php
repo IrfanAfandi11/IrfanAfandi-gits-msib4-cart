e181adace00cb909fb58ec89ea3c45306cd092e8
ayusekar94 <ayu.wiranti181@gmail.com>
2023-04-11 11:53:18 +0700

    update dashbord

--- resources/views/dashbord/partials/footer.blade.php
+++ resources/views/dashbord/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Ananda Ayu Sekar &copy; Your Website 2023</div>
+            <div class="text-muted">Kelompok 15 &copy; Your Website 2023</div>
             <div>
                 <a href="#">Privacy Policy</a>
                     &middot;
